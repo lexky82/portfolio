4bf6ccb1030cf85a8260a4df6b439005f007a6ce
Kwon Hyeok Jin <lexky@naver.com>
2021-07-22 13:12:10 +0900

    fix: 이력서 수정으로 인한 google 문서 주소변경

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://drive.google.com/file/d/1fu_4Q6WrTfsydRubweKvGjYcB6nFFDZ6/view', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/187goWo0g9nbzFaqUyro65bYdaqXwveIq/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
